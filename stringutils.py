ae10045193536214ec90bb026246bc24d57ebb50
Luca Soldaini <luca@soldaini.net>
2016-08-22 14:17:41 -0400

    added new string distances metrics

--- stringutils.py
+++ stringutils.py
@@ -1,5 +1,6 @@
 import re
 import string
+from nltk.stem.snowball import EnglishStemmer
 
 def len_utf8(s):
     """Return the length of s in bytes"""
@@ -40,6 +41,11 @@ class SimpleTokenizer(object):
                 yield tok
 
 
+def make_ngrams(s, n):
+    s = u'{out}{s}{out}'.format(out='$'*(n-1), s=s)
+    return (s[i:i+n] for i in range(len(s) - n + 1))
+
+
 def string_similarity(x, y, similarity_name, n=3):
     """Calculate approximate string similarity; for convinience reasons,
     strings are split into n-grams"""
@@ -51,7 +57,30 @@ def string_similarity(x, y, similarity_name, n=3):
     elif similarity_name == 'cosine':
         return len(X & Y) / numpy.sqrt(len(X) * len(Y))
     elif similarity_name == 'overlap':
-        return len(X & Y)
+        return len(X & Y) / max(len(X), len(Y))
+    elif similarity_name == 'levenshtein':
+        return 1 - levenshtein(x, y) / max(len(x), len(y))
     else:
         msg = 'Similarity {} not recognized'.format(similarity_name)
         raise TypeError(msg)
+
+
+def levenshtein(s1, s2):
+    if len(s1) < len(s2):
+        return levenshtein(s2, s1)
+
+    # len(s1) >= len(s2)
+    if len(s2) == 0:
+        return len(s1)
+
+    previous_row = range(len(s2) + 1)
+    for i, c1 in enumerate(s1):
+        current_row = [i + 1]
+        for j, c2 in enumerate(s2):
+            insertions = previous_row[j + 1] + 1 # j+1 instead of j since previous_row and current_row are one character longer
+            deletions = current_row[j] + 1       # than s2
+            substitutions = previous_row[j] + (c1 != c2)
+            current_row.append(min(insertions, deletions, substitutions))
+        previous_row = current_row
+
+    return previous_row[-1]
