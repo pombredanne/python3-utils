145dde8f595b171ef64629a029027f04bfaa6ce7
Luca Soldaini <luca@soldaini.net>
2017-01-16 13:41:51 -0500

    added nice funciton to visualize layers

--- keras.py
+++ keras.py
@@ -1,8 +1,15 @@
+# built-in modules
+import os
+
 # installed modules
 import numpy
 from keras.engine import Layer
 from keras.callbacks import Callback
 import keras.backend as K
+import h5py
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
 # project modules
 from .meta import time_formatter, timer
@@ -47,3 +54,39 @@ class NotSoChattyLogger(Callback):
             self._partial_counter = 0
 
         self.losses.append(loss)
+
+
+def plot_weights(model, dest_dir, layers=None):
+    for layer in model.layers:
+        if layers is not None and layer not in layers:
+            continue
+
+        try:
+            W, b = model.get_layer(layer.name).get_weights()
+        except ValueError:
+            # this is not a model with weights (e.g. noise layer)
+            continue
+
+        range = max(
+            numpy.max(W), numpy.abs(numpy.min(W)),
+            numpy.max(b), numpy.abs(numpy.min(b)),
+        )
+
+        b = b.reshape(b.shape[0], 1) @ numpy.ones((1, 10))
+
+        fig, (ax1, ax2) = plt.subplots(1, 2, sharey=False)
+
+        fig.suptitle('Weights {}'.format(layer.name))
+
+        ax1.imshow(W, cmap='coolwarm', vmin=-range, vmax=range, interpolation='none')
+        cax = ax2.imshow(b, cmap='coolwarm', vmin=-range, vmax=range, interpolation='none')
+
+        cbar = fig.colorbar(cax, ticks=[-range, 0, range])
+        cbar.ax.set_yticklabels(['{:.1e}'.format(-range), ' 0', '{:.1e}'.format(range)])
+
+        output_fn = os.path.join(
+            dest_dir, '{}-{}.pdf'.format(model.name, layer.name)
+        )
+        fig.savefig(output_fn)
+        fig.clf()
+
