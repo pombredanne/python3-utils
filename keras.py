4d6e4c8a88306ac2adef95a57138267d13b3e674
Luca Soldaini <luca@soldaini.net>
2017-01-19 10:52:58 -0500

    switched to official keras epsilon value

--- keras.py
+++ keras.py
@@ -18,18 +18,25 @@ from .meta import time_formatter, timer
 
 class NotSoChattyLogger(Callback):
     def __init__(self, print_every=1000000,
-                 start=None, counter=None, metrics=None):
+                 start=None, counter=None, metrics=None,
+                 custom_metrics=None):
         self._print_every = print_every
         self._partial_counter = 0
         self._total_counter = counter if counter is not None else 0
         self._start = start if start is not None else timer()
         self._metrics = ['loss'] if metrics is None else list(metrics)
+        self._custom_metrics = {
+            m: [] for m in (custom_metrics if custom_metrics else {})
+        }
 
         super(NotSoChattyLogger, self).__init__()
 
     @property
     def start(self):
         return self._start
+
+    def __getitem__(self, item):
+        return self._custom_metrics[item]
 
     @property
     def count(self):
@@ -40,10 +47,15 @@ class NotSoChattyLogger(Callback):
         self._total_counter += logs.get('size', 0)
 
         if self._partial_counter >= self._print_every:
+            custom_logs = dict(logs)
+            custom_logs.update(
+                {k: v[-1] for k, v in self._custom_metrics.items() if v}
+            )
+
             delta = timer() - self._start
             metrics = ' - '.join(
-                '{}: {:.1e}'.format(m, float(logs.get(m)))
-                for m in self._metrics if m in logs
+                '{}: {:.1e}'.format(m, float(custom_logs.get(m)))
+                for m in self._metrics if m in custom_logs
             )
 
             print(
@@ -55,7 +67,7 @@ class NotSoChattyLogger(Callback):
             self._partial_counter = 0
 
 
-def plot_weights(model, dest_dir, layers=None, dead_threshold=1e-8):
+def plot_weights(model, dest_dir, layers=None):
     for layer in model.layers:
         if layers is not None and layer not in layers:
             continue
@@ -67,7 +79,7 @@ def plot_weights(model, dest_dir, layers=None, dead_threshold=1e-8):
             continue
 
         dead_connections = sum(
-            1 if w < dead_threshold else 0 for w in numpy.nditer(W)
+            1 if w < K.epsilon() else 0 for w in numpy.nditer(W)
         )
         dead_connections_percent = dead_connections / numpy.prod(W.shape)
 
